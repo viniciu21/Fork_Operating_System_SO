5e64366e08877c01060eab94f9918c292d47f44a
paulovanzo <paulovanzo@ufrn.edu.br>
2021-01-30 01:01:59 -0300

    Terminando com arquivo txt

--- Fork.c
+++ Fork.c
@@ -18,25 +18,39 @@ void await(int t){
 int main(){
 
     pid_t filho1,filho2,neto1,neto2;
+    time_t mortes[5];
+    time_t nascimento[5];
+    FILE *file;
 
-    time_t start = time(NULL);
+    file = fopen("mortes.txt","w");
+
+    if(!file){
+        printf("Erro ao abrir arquivo\n");
+        exit(-1);
+    }
+
+    nascimento[0] = time(NULL);
 
     await(14);
 
     //primeiro filho 14s
 
-    printf("PAI (PID=%d)\nProcesso Filho #1 criado tempo: %ld\n", getpid(),time(NULL)-start);
+    nascimento[1] = time(NULL);
+
+    printf("PAI (PID=%d)\n", getpid());
+    printf("Nascimento Filho #1: %ld\n", nascimento[1]);
 
     filho1 = fork();
 
     if(filho1 < 0){
-        fprintf(stderr,"Erro ao criar processo filho #1");
+        fprintf(stderr,"Erro ao criar processo filho #1\n");
         exit(-1);
     }else{
         if(filho1 > 0){
             //segundo filho 16s
             await(2);
-            printf("Processo Filho #2 criado tempo: %ld\n", time(NULL)-start);
+            nascimento[2] = time(NULL);
+            printf("Nascimento Filho #2: %ld\n", nascimento[2]);
             filho2 = fork();
             if(filho2 < 0){
                 fprintf(stderr,"Erro ao criar processo neto #2\n");
@@ -45,21 +59,26 @@ int main(){
                 if(filho2 == 0){
                     //segundo neto 30s
                     await(14);
-                    printf("Filho #2(PID=%d)\tNeto #2 tempo: %ld\n",getpid(),time(NULL)-start);
+                    nascimento[4] = time(NULL);
+                    printf("Filho #2(PID=%d)\tNascimento Neto #2 Hora: %ld\n",getpid(),nascimento[4]);
                     neto2 = fork();
                     if(neto2 < 0){
                         fprintf(stderr,"Erro ao criar processo neto #2");
                         exit(-1);
                     }else{
                         if(neto2 == 0){
-                            // morte do segundo neto 48s.
+                            // morte do segundo neto 18s.
                             await(18);
-                            printf("Neto #2(PID=%d)\ttempo de morte cronológico a partir do pai: %ld\n", getpid(),time(NULL)-start);
+                            mortes[4] = time(NULL);
+                            printf("Neto #2(PID=%d)\tHora de morte: %ld\tTempo vivido: %.1lf\tNascimento: %ld\n", getpid(),mortes[4], difftime(mortes[4],nascimento[4]), nascimento[4]);
+                            fprintf(file, "Neto 2, tempo vivido: %.1lf\n", difftime(mortes[4],nascimento[4]));
                             exit(0);
                         }else{
                             await(16);
                             // morte do segundo filho 30s, await(16+14)
-                            printf("Filho #2\t\ttempo de morte cronológico a partir do pai: %ld\n", time(NULL)-start);
+                            mortes[2] = time(NULL);
+                            printf("Filho #2\t\tHora de morte: %ld\tTempo vivido: %.1lf\tNascimento: %ld\n", mortes[2], difftime(mortes[2],nascimento[2]), nascimento[2]);
+                            fprintf(file, "Filho 2, tempo vivido: %.1lf\n", difftime(mortes[2],nascimento[2]));
                             exit(0);
                         }
                     }
@@ -68,7 +87,8 @@ int main(){
         }else{
             //primeiro neto 26s
             await(12);
-            printf("Filho #1(PID=%d)\tNeto #1 tempo: %ld \n",getpid(),time(NULL)-start);
+            nascimento[3] = time(NULL);
+            printf("Filho #1(PID=%d)\tNascimento Neto #1 Hora: %ld\n",getpid(),nascimento[3]);
             neto1 = fork();
             if(neto1 < 0){
                 fprintf(stderr,"Erro ao criar processo neto #1\n");
@@ -77,14 +97,18 @@ int main(){
                 if(neto1 == 0){
                     // morte do primeito neto 38s
                     await(12);
-                    printf("Neto #1(PID=%d)\ttempo de morte cronológico a partir do pai: %ld\n", getpid(),time(NULL)-start);
+                    mortes[3] = time(NULL);
+                    printf("Neto #1(PID=%d)\tHora de morte: %ld\tTempo vivido: %.1lf\tNascimento: %ld\n", getpid(),mortes[3], difftime(mortes[3],nascimento[3]), nascimento[3]);
+                    fprintf(file, "Neto 1, tempo vivido: %.1lf\n", difftime(mortes[3],nascimento[3]));
                     exit(0);
                 }else{
                     //morte do primeiro filho 44s
                     await(18);
                     // morre com 30s, await(18+12)
                     wait(NULL);
-                    printf("Filho #1\t\ttempo de morte cronológico a partir do pai: %ld\n", time(NULL)-start);
+                    mortes[1] = time(NULL);
+                    printf("Filho #1\t\tHora de morte: %ld\tTempo vivido: %.1lf\tNascimento: %ld\n", mortes[1], difftime(mortes[1],nascimento[1]), nascimento[1]);
+                    fprintf(file, "Filho 1, tempo vivido: %.1lf\n", difftime(mortes[1],nascimento[1]));
                     exit(0);
                 }
             }
@@ -95,9 +119,10 @@ int main(){
 
     await(16);
 
-    if(filho1>0){
-        printf("Tempo de morte do pai: %ld\n", time(NULL) - start);
-    }
+    mortes[0] = time(NULL);
+
+    printf("Hora de morte do pai:\t%ld\t\tTempo vivido: %.1lf\tNascimento: %ld\n", mortes[0],difftime(mortes[0],nascimento[0]), nascimento[0]);
+    fprintf(file, "Pai, tempo vivido: %.1lf\n", difftime(mortes[0],nascimento[0]));
     
     return 0;
 
